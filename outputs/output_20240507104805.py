b77d813af9fbfc66dc8086f74aea14e1c39fc207
manthankumar12 <manthan.kumar@progressresidential.com>
2024-05-07 10:48:06 +0000

    Extracted source code from YAML

---
+++ outputs/output_20240507104805.py
@@ -0,0 +1,27 @@
+import snowflake.connector
+import pandas as pd
+from snowflake.connector import DictCursor
+from snowflake.snowpark.session import Session 
+from snowflake.snowpark import DataFrame 
+from snowflake.snowpark.functions import col
+
+def hello(session: Session) -> DataFrame:
+    df = session.table("SNOWLENS.DEMO.QUERY_HISTORY_TABLE")
+    return df
+
+# Define connection parameters
+connection_params = {
+    "ACCOUNT": "ls31517.ap-southeast-1",
+    "USER": "manthan",
+    "PASSWORD": "Manthanrana123",
+    "ROLE": "ACCOUNTADMIN",
+    "WAREHOUSE": "COMPUTE_WH",
+    "DATABASE": "SNOWLENS",
+    "SCHEMA": "DEMO"
+}
+
+# Create a Snowpark session using the defined connection parameters
+session = Session.builder.configs(connection_params).create()
+
+# Fetch the table and display it
+print(hello(session).show())
