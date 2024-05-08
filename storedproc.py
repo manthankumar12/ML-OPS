41c3634a3ba0a199f714ad347edd8351645f7f86
manthankumar12 <166620532+manthankumar12@users.noreply.github.com>
2024-05-08 19:36:37 +0530

    Update storedproc.py

--- storedproc.py
+++ storedproc.py
@@ -1,20 +1,8 @@
 # storedproc.py file
-import snowflake.connector
-from snowflake.snowpark.session import Session 
 
 def run(message):    
   return "hello world, v1";
 
-connection_params = {
-    "ACCOUNT": "ls31517.ap-southeast-1",
-    "USER": "manthan",
-    "PASSWORD": "Manthanrana123",
-    "ROLE": "ACCOUNTADMIN",
-    "WAREHOUSE": "COMPUTE_WH",
-    "DATABASE": "SNOWLENS",
-    "SCHEMA": "DEMO"
-}
 
-if __name__ == "__main__":
-    session = Session.builder.configs(connection_params).create()
-    print (hello (session).show())
+
+
