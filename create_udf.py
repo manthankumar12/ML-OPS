880eaa5a81662af9ec1528e0edec64f95ae8460e
manthankumar12 <166620532+manthankumar12@users.noreply.github.com>
2024-06-28 20:08:10 +0530

    Update create_udf.py

--- create_udf.py
+++ create_udf.py
@@ -2,18 +2,8 @@ import snowflake.connector
 import os
 
 def create_udf():
-    # Connection parameters
-    # conn_params = {
-    #     "user": os.getenv('SNOWFLAKE_USER'),
-    #     "password": os.getenv('SNOWFLAKE_PASSWORD'),
-    #     "account": os.getenv('SNOWFLAKE_ACCOUNT'),
-    #     "warehouse": os.getenv('SNOWFLAKE_WAREHOUSE'),
-    #     "role": os.getenv('SNOWFLAKE_ROLE'),
-    #     "database": os.getenv('SNOWFLAKE_DATABASE'),
-    #     "schema": os.getenv('SNOWFLAKE_SCHEMA')
-    # }
-
-     conn_params = {
+    
+    conn_params = {
         "user": "${{ secrets.SNOWFLAKE_USER }}",
         "password": "${{ secrets.SNOWFLAKE_PASSWORD }}",
         "account": "${{ secrets.SNOWFLAKE_ACCOUNT }}",
@@ -34,29 +24,29 @@ connection = snowflake.connector.connect(
 )
 
     # Read the Python function to be used in UDF
-    with open('outputs/turnover_forecast.py', 'r') as file:
-        udf_code = file.read()
+with open('outputs/turnover_forecast.py', 'r') as file:
+    udf_code = file.read()
 
-    # SQL command to create the UDF
-    create_function_sql = f"""
-    CREATE OR REPLACE FUNCTION predict_turnover_udf()
-    RETURNS TABLE (NET_TURNOVER FLOAT, LABEL INT)
-    LANGUAGE PYTHON
-    RUNTIME_VERSION = '3.8'
-    PACKAGES = ('snowflake-snowpark-python', 'pandas', 'scikit-learn')
-    HANDLER = 'turnover_forecast.predict_turnover'
-    AS
-    $$
-    {udf_code}
-    $$;
-    """
+# SQL command to create the UDF
+create_function_sql = f"""
+CREATE OR REPLACE FUNCTION predict_turnover_udf()
+RETURNS TABLE (NET_TURNOVER FLOAT, LABEL INT)
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.8'
+PACKAGES = ('snowflake-snowpark-python', 'pandas', 'scikit-learn')
+HANDLER = 'turnover_forecast.predict_turnover'
+AS
+$$
+{udf_code}
+$$;
+"""
 
     # Execute the SQL command
-    with connection.cursor() as cursor:
-        cursor.execute(create_function_sql)
-        print("Function created successfully")
+with connection.cursor() as cursor:
+    cursor.execute(create_function_sql)
+    print("Function created successfully")
 
-    # Close the connection
+# Close the connection
     connection.close()
 
 if __name__ == "__main__":
