455c48ed42d93ab6827ff15deb5ff6f928c09de5
manthankumar12 <166620532+manthankumar12@users.noreply.github.com>
2024-06-28 20:19:38 +0530

    Update create_udf.py

--- create_udf.py
+++ create_udf.py
@@ -25,30 +25,30 @@ def create_udf():
      )
 
     # Read the Python function to be used in UDF
-with open('outputs/turnover_forecast.py', 'r') as file:
-    udf_code = file.read()
+     with open('outputs/turnover_forecast.py', 'r') as file:
+         udf_code = file.read()
 
 # SQL command to create the UDF
-create_function_sql = f"""
-CREATE OR REPLACE FUNCTION predict_turnover_udf()
-RETURNS TABLE (NET_TURNOVER FLOAT, LABEL INT)
-LANGUAGE PYTHON
-RUNTIME_VERSION = '3.8'
-PACKAGES = ('snowflake-snowpark-python', 'pandas', 'scikit-learn')
-HANDLER = 'turnover_forecast.predict_turnover'
-AS
-$$
-{udf_code}
-$$;
-"""
+     create_function_sql = f"""
+     CREATE OR REPLACE FUNCTION predict_turnover_udf()
+     RETURNS TABLE (NET_TURNOVER FLOAT, LABEL INT)
+     LANGUAGE PYTHON
+     RUNTIME_VERSION = '3.8'
+     PACKAGES = ('snowflake-snowpark-python', 'pandas', 'scikit-learn')
+     HANDLER = 'turnover_forecast.predict_turnover'
+     AS
+     $$
+     {udf_code}
+     $$;
+     """
 
     # Execute the SQL command
-with connection.cursor() as cursor:
-    cursor.execute(create_function_sql)
-    print("Function created successfully")
+     with connection.cursor() as cursor:
+         cursor.execute(create_function_sql)
+         print("Function created successfully")
 
 # Close the connection
-    connection.close()
+     connection.close()
 
 if __name__ == "__main__":
     create_udf()
